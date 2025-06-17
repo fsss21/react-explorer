a5cc32cd1400a3db4a6d5d9486ec69ba766f7b95
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2025-06-17 10:15:46 +0300

    fix: content

--- src/data/data.js
+++ src/data/data.js
@@ -18,4 +18,47 @@ export const translations = {
   }
 };
 
-export const operationStyles = {};
+export const operationStyles = {
+  eon: {
+    position: 'absolute',
+    bottom: '25%',
+    left: '3%',
+    width: '400px',
+    height: '160px',
+
+    cursor: 'pointer'
+  },
+  chuh: {
+    position: 'absolute',
+    bottom: '22%',
+    right: '14%',
+    width: '540px',
+    height: '160px',
+    cursor: 'pointer'
+  },
+  ice: {
+    position: 'absolute',
+    top: '37%',
+    right: '29%',
+    width: '640px',
+    height: '180px',
+    cursor: 'pointer'
+  },
+  unkers: {
+    position: 'absolute',
+    top: '10%',
+    left: '20%',
+    width: '690px',
+    height: '250px',
+    cursor: 'pointer'
+  },
+  fly: {
+    position: 'absolute',
+    bottom: '22%',
+    left: '25%',
+    width: '600px',
+    height: '180px',
+
+    cursor: 'pointer'
+  }
+};
